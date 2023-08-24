0eb90b3c87c2da28347f7df1fbdac33ecf1bfbda
Walt Yao <walt.yao@gmail.com>
2023-08-25 00:38:07 +0800

    UI changes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const fs = require('fs').promises
+
+const users = []
 
 const app = express()
 const port = 3000
 
 app.set('view-engine', 'ejs')
-// app.set('views', __dirname + '/views/')
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/public/'))
 
 app.get('/', async (req, resp) => {
@@ -13,6 +16,31 @@ app.get('/', async (req, resp) => {
     // resp.send(await fs.readFile('./home.html', 'utf8'))
     
     resp.render('home.ejs')
+})
+
+app.post('/login', async (req, resp) => {
+
+    req.body.username
+    req.body.email
+    req.body.password 
+    
+    resp.render('home.ejs')
+})
+
+app.post('/register', async (req, resp) => {
+
+    try {
+        const hashedPassword = bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString,
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        resp.redirect('/login')
+    } catch {
+        resp.redirect('/')
+    }
 })
 
 app.get('/dashboard', async (req, resp) => {
