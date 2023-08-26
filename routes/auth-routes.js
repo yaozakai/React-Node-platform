36a807a16a56538122770ee33c3962b90185f969
Walt Yao <walt.yao@gmail.com>
2023-08-26 23:19:04 +0800

    Client-side registration validation

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,11 +2,15 @@ const check_auth = require('./auth-check')
 const router = require('express').Router()
 const passport = require('passport')
 const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 const User = require('./../db/user-model')
+
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_KEY)
 
 
 router.get('/login', (req, resp) => {
-    resp.render('/')
+    resp.redirect('/')
 })
 
 router.post('/login', check_auth.not, passport.authenticate('local', {
@@ -26,27 +30,78 @@ router.post('/register', check_auth.not, async (req, resp) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         // check if user exists
-        User.findOne({ where: {username: req.body.username}}).then((currentUser) => {
-            User.findOne({ where: {email: req.body.email}}).then((emailFound) => {
+        User.findOne({ username: req.body.username }).then((currentUser) => {
+            User.findOne({ email: req.body.email}).then((emailFound) => {
                 if (currentUser || emailFound){
                     // user exists
+                    req.flash('alertTitle', 'Confirmation email resent!')
+                    req.flash('alertMsg', 'Your account already exists, so we sent a confirmation email to ' + req.body.email + '. Please verify your account through the link sent to you.')
                     resp.redirect('/')
                 } else {
                     // create user
                     new User({
                         username: req.body.username,
                         email: req.body.email,
+                        emailToken: crypto.randomBytes(64).toString('hex'),
                         googleId: '',
                         image: '',
                         password: hashedPassword
                     }).save().then((newUser) => {
-                        resp.redirect('/dashboard')
+
+                        const msg = {
+                            from: 'no-reply@waltyao.com',
+                            to: req.body.email,
+                            subject: 'Please confirm your account',
+                            text: `Thanks for stopping by, click on this link to confirm: 
+                            http://${req.headers.host}/auth/email?token=${newUser.emailToken}
+                            `,
+                            html: `<a href="http://${req.headers.host}/auth/email?token=${newUser.emailToken})">`
+                        }
+                        sgMail.send(msg).then(() => {
+                            req.flash('alertTitle', 'Check your email!')
+                            req.flash('alertMsg', 'Email has been sent to ' + req.body.email + '. Please verify your account through the link sent to you.')
+                            resp.redirect('/')
+                        }).catch((error) => {
+                            req.flash('alertTitle', 'Email error')
+                            req.flash('alertMsg', 'Something when wrong when sending email. Please try again.')
+                        }) 
                     })
                 }
             }) 
         })
     } catch {
         resp.redirect('/')
+    }
+})
+
+router.get('/email', async(req, resp, next) => {
+
+    try {
+        User.findOne({ emailToken: req.query.token }).then((currentUser) => {
+            // req.flash('alertTitle', 'Account confirmed!')
+            // req.flash('alertMsg', 'Logging in..')
+            currentUser.emailToken = NULL
+            currentUser.isVerified = true
+            currentUser.save()
+            req.login(currentUser).then(() => {
+                req.flash('alertTitle', `Hi ${currentUser.username}!`)
+                req.flash('alertMsg', 'Welcome to the site for the first time! 😎')
+                const redirectURL = req.session.redirectTo || '/dashboard'
+                delete req.session.redirectTo
+                resp.redirect(redirectURL)
+            }).catch((error) => {
+                req.flash('alertTitle', 'Login error')
+                req.flash('alertMsg', 'Something.')
+            })
+            return resp.redirect('/')
+        }).catch((error) => {
+            req.flash('alertTitle', 'Email error')
+            req.flash('alertMsg', 'Email token is invalid, please try registering again.')
+            return resp.redirect('/')
+        })
+        
+    } catch(error) {
+
     }
 })
 
--- server.js
+++ server.js
@@ -61,51 +61,13 @@ initializePassport(
     passport,
     email => User.findOne({email: email}).email,
     id => User.findOne({googleId: id}.id)
-    // email =>  users.find(user => user.email === email),
-    // id => users.find(user => user.id === id)
 )
 
 app.get('/', check_auth.not, (req, resp) => {
-    // resp.send(await fs.readFile('./home.html', 'utf8'))
+    // req.flash('success', 'failedddfsdfsd')
+
     resp.render('home.ejs')
 })
-
-// app.post('/login', check_auth.not, passport.authenticate('local', {
-//     successRedirect: '/dashboard',
-//     failureRedirect: '/',
-//     failureFlash: true
-//  }))
-
-// app.post('/register', check_auth.not, async (req, resp) => {
-
-//     try {
-//         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-//         users.push({
-//             id: Date.now().toString(),
-//             username: req.body.username,
-//             email: req.body.email,
-//             password: hashedPassword
-//         })
-//         resp.redirect('/')
-//     } catch {
-//         resp.redirect('/')
-//     }
-//     console.log(users)
-// })
-
-// app.delete('/logout', (req, resp) => {
-//     req.logout(function(err) {
-//         if (err) { return next(err) }
-//         resp.redirect('/')
-//     })
-    
-
-// })
-
-
-
-
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
